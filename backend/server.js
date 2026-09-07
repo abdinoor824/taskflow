b089495c1e2450de0e6a0a7845bae40fc167d43b
kune-fx <abdinurkune@gmail.com>
2026-09-07 18:51:54 +0300

    update cors

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,10 @@ const app = express();
 
 connectDB();
 
+// app.use(cors({
+//   origin: 'http://localhost:3000',
+//   credentials: true,
+// }));
 app.use(cors({
   origin: 'https://taskflow-git-main-abdinoorahmeds-projects.vercel.app',
   credentials: true,
