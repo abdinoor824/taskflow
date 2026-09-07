5bd868f275408576cf92912ff0d99b6ac160b5dc
kune-fx <abdinurkune@gmail.com>
2026-09-07 18:06:14 +0300

    use env var

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:5000/api';
+const API_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000/api';
 
 export async function apiFetch(path, options = {}) {
   const res = await fetch(`${API_URL}${path}`, {
