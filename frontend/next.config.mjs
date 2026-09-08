908412abf75754c45aed6238c09356f329baa223
kune-fx <abdinurkune@gmail.com>
2026-09-08 21:04:28 +0300

    consolidated next config

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /* config options here */
+   async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'https://taskflow-vxqe.onrender.com/api/:path*',
+      },
+    ];
+  },
 };
+
 
 export default nextConfig;
